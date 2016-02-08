aa596d9428ce137d3a272f7d8158c7c31fcce436
Ben Walters <bwalter4@hawk.iit.edu>
2016-02-08 11:06:26 +0000

    working on graph data structure

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <list>
 
 int main()
 {
 	std::cout << "I am graph processing system!\n";
+	std::list<int> linkedList;
+	linkedList.push_back(3);
+	linkedList.push_back(4);
+}
+
+class Graph {
+public:
+	std::list<GraphNode> vertices;
+	void addVertex(GraphNode node);
+};
+
+Graph::addVertex (GraphNode node) {
+	vertices.push_back(node);
+}
+
+class GraphNode {
+public:
+	std::list<GraphNode> neighbors;
+	int weight;
 }
