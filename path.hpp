18ee9c00f9823db20a73e9526a98293f844589c3
gnu3ra <aballmer42@gmail.com>
2016-02-20 11:58:57 -0600

    Added path class

---
+++ path.hpp
@@ -0,0 +1,22 @@
+#include "node.hpp"
+#include <vector>
+
+
+class Path {
+
+
+public:
+  Node startNode();
+  Node endNode();
+  std::vector<Relationship> getRelationships();
+  std::vector<Relationship> getRelationshipsReverse(); 
+  Relationship lastRelationship(); //last relation in vector
+  int getLength();
+private:
+  Node start;
+  Node end;
+  int length;
+  std::vector<Node> nodeList;
+  
+
+};
