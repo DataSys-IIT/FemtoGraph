b34203a0fd101b472c05232aff2f29c7adeea654
gnu3ra <aballmer42@gmail.com>
2016-02-20 11:40:13 -0600

    Basic node api headers

---
+++ node.hpp
@@ -0,0 +1,47 @@
+#include <vector>
+
+/* Some classes representing nodes, edges, and
+ * associated data.
+ */ 
+
+class Node {
+
+public:
+  virtual createRelationship(Relationship R);
+  Data getData();
+  void setData(Data d);
+private:
+  std::vector<Relationship> relationships;
+  Data datavalue;
+
+};
+
+
+/* represents a relationship between nodes */ 
+class Relationship {
+
+public:
+  Node to;
+  Node from;
+  Relationship();
+  ~Relationship();
+  Data getData();
+  void setData(Data d);
+
+private: 
+  Data datavalue;
+};
+
+/* generic data template for edges and vertecies */ 
+template <typename T> class Data {
+
+public:
+  virtual Data();
+  ~Data();
+  T getData();
+  void setData(T dat);
+
+private:
+  std::vector<T> datalist;
+  
+};
