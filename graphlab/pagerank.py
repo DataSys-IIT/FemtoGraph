160f36a1f01f46a25b77b7c118411e38bd47bb18
Ben Walters <bwalter4@hawk.iit.edu>
2016-05-09 18:52:34 +0000

    results and fixed graphlab command args

--- graphlab/pagerank.py
+++ graphlab/pagerank.py
@@ -3,13 +3,20 @@ import graphlab
 import sys
 import time
 
+if (len(sys.argv) < 5):
+	print "Usage: %s <edge list filename> <epsilon> <damping factor> <max iterations>" % sys.argv[0]
+	sys.exit(1)
+
 filename = sys.argv[1]
 print(filename)
 
 # from https://dato.com/products/create/docs/generated/graphlab.pagerank.create.html
+epsilon = float(sys.argv[2])
+damping = float(sys.argv[3])
+maxIterations = int(sys.argv[4])
 g = graphlab.load_graph(filename, format='snap')
 start = time.time()
-pr = graphlab.pagerank.create(g, reset_probability=.5, max_iterations=10, _distributed=False)
+pr = graphlab.pagerank.create(g, reset_probability=damping, max_iterations=maxIterations, _distributed=False, threshold=epsilon)
 end = time.time()
 pr_out = pr['pagerank']     # SFrame
 g.vertices['pagerank'] = pr['graph'].vertices['pagerank']
